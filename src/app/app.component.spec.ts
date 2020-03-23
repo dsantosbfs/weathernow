2bcf3fbd016528b48472904c2574538ded66285b
Danilo Santos <dsantosbfs@gmail.com>
2020-03-22 21:41:04 -0300

    test(app): fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import { CardModule } from 'weather-lib/';
+import { CardModule, DataLabelModule } from 'weather-lib/';
 import { StoreModule } from '@ngrx/store';
-import { DataLabelModule } from 'weather-lib/public-api';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
