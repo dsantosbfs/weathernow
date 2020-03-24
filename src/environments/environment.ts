2986c44ce9d245a918e1cb990426ed395c817efb
Danilo Santos <dsantosbfs@gmail.com>
2020-03-24 18:00:48 -0300

    doc(app): implement readme documentation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   apiUrl: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5',
-  clientId: 'd1398fc9fadc143c7dc78f13d8d0914b',
+  clientId: 'YOUR_OPEN_WEATHER_API_KEY',
   production: false,
 };
 
